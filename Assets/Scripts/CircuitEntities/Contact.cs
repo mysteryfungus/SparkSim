aa038018ea9c7f6da8aa382058c5d9671e5918cd
mysteryfungus <46565319+mysteryfungus@users.noreply.github.com>
2025-05-08 00:28:45 +0700

    Connection system in progress

--- Assets/Scripts/CircuitEntities/Contact.cs
+++ Assets/Scripts/CircuitEntities/Contact.cs
@@ -1,7 +1,17 @@
+using System;
 using UnityEngine;
 
 public class Contact : MonoBehaviour
 {
+    public GameObject Self {private set;  get; }
+    public string Name {private set; get; }
+    public UInt32 ID {private set; get; }
+
+    void Awake()
+    {
+        ID = IDManager.AssignId();
+    }
+
     void Start()
     {
         
@@ -10,5 +20,10 @@ public class Contact : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnDestroy()
+    {
+        IDManager.RemoveId(ID);
     }
 }
